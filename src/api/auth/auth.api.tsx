d7f112ebe31ca57adbed8e8b4091d57249e1fddc
MKhalid-sirius <mkhalid@sirius-its.com>
2024-03-07 11:32:14 +0200

    finish reset password

--- src/api/auth/auth.api.tsx
+++ src/api/auth/auth.api.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ClientCalls } from "../../utils/client-calls/ClientCalls";
 
 export class SignIn {
@@ -19,5 +20,22 @@ export class Signup {
     phone: string;
   }): Promise<any> => {
     return (await ClientCalls.post("auth/signup", data)).data;
+  };
+}
+
+export class Token {
+  static sendToken = async (data: {
+    email: string;
+  }): Promise<any> => {
+    return (await ClientCalls.post("auth/new-token", data)).data;
+  };
+}
+
+export class Password {
+  static reset = async (token:string, data: {
+    password: string;
+    confirmPassword: string;
+  }): Promise<any> => {
+    return (await ClientCalls.post(`auth/new-password?token=${token}`, data)).data;
   };
 }
